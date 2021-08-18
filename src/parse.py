d4867b87d5c28f569c8f5b6e8def42dafacae77a
Andrii Kovalyk <andreykovalyk30@gmail.com>
2021-08-18 21:06:47 +0300

    Added file with requirements and some comments

--- src/parse.py
+++ src/parse.py
@@ -2,13 +2,16 @@ import requests
 import sqlite3 as sql
 from datetime import datetime
 
+# convert regular time format to seconds
 dtn = datetime.now()
 dtn = dtn.replace(microsecond=0)
 today = datetime.strptime(str(dtn), "%Y-%m-%d %H:%M:%S")
 start = datetime(1970, 1, 1)
 
+# calculate amount of seconds from 1970 year for nowadays
 date = int((today - start).total_seconds())
 
+# Companies which raise in rest api implementation
 companies = {
     "PD": f"https://query1.finance.yahoo.com/v7/finance/download/PD?period1=1554940800&period2={date}&interval=1d&events=history&includeAdjustedClose=true",
     "ZUO": f"https://query1.finance.yahoo.com/v7/finance/download/ZUO?period1=1523491200&period2={date}&interval=1d&events=history&includeAdjustedClose=true",
@@ -19,12 +22,14 @@ companies = {
     "RUN": f"https://query1.finance.yahoo.com/v7/finance/download/RUN?period1=1438732800&period2={date}&interval=1d&events=history&includeAdjustedClose=true",
 }
 
+# Web-site URL and Headers
 URL = "https://query1.finance.yahoo.com/v7/finance/download/"
 HEADERS = {
     "accept": "text/javascript, application/json, text/html, application/xml, text/xml, */*",
     "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
 }
 
+# connecting and creating sqlite3 database
 connect = sql.connect("data/finance.db")
 cursor = connect.cursor()
 cursor.execute(
@@ -41,7 +46,7 @@ cursor.execute(
 )"""
 )
 
-
+# populate database with finance data
 def add_to_db(name, fields):
     cursor.execute(
         f"""SELECT * FROM companies WHERE Date = '{fields[0]}' AND Name = '{name}'"""
@@ -55,7 +60,9 @@ def add_to_db(name, fields):
         connect.commit()
 
 
+# requests to downloading the data from web-page
 for company in companies:
+    # responses from server
     response = requests.get(
         companies[company],
         headers=HEADERS,
