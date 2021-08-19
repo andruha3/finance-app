2f9f713b2999cef00432e3f3766e64cc0a4771fc
Andrii Kovalyk <andreykovalyk30@gmail.com>
2021-08-19 17:59:19 +0300

    Some changes in Dockerfile

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from src import app
 
 if __name__ == "__main__":
-    with open("src/parse.py", "r") as f:
-        exec(f.read())
     app.run(debug=True, host="0.0.0.0")
---
+++ start.sh
@@ -0,0 +1,2 @@
+python src/parse.py 
+python app.py
